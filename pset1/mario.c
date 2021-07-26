864c027d94ba9f47c152869896cda092344acfb0
AlexanderPkv2 <OoLAVA2281337@mail.ru>
2021-07-26 15:45:45 +0300

    added task2

--- pset1/mario.c
+++ pset1/mario.c
@@ -1,6 +1,95 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
+	
+
+	int heigth;
+
+	//Are the input parametrs correct?
+	while(true) 
+	{
+	  printf("Choose piramids heigth (1 - 23 range): ");
+	  scanf("%i",&heigth);
+
+	  if (heigth >= 1 && heigth <= 23)
+	  {
+	  	break;
+	  }
+
+	 printf("Out of range\n"); 
+	}
+
+	printf("\n");
+
+	//First piramid
+	for (int i = 0; i < heigth; i++)
+	{
+		//Writes " "
+        for (int j = heigth-1; j > i ; j--)
+		{
+			printf(" ");
+		}
+
+		//Writes #
+		for (int j = 0; j < i+2 ; j++)
+		{
+			printf("#");
+		}
+
+		printf("\n");
+	}
+
+	printf("\n");
+
+	//Second piramid
+	for (int i = 0; i < heigth; i++)
+	{
+	
+		//Writes #
+		for (int j = 0; j < i+2 ; j++)
+		{
+			printf("#");
+		}
+
+		printf("\n");
+	}
+
+	printf("\n");
+
+	//Third piramid
+	for (int i = 0; i < heigth; i++)
+	{
+	
+		//Writes #
+		for (int j = heigth+1; j > i ; j--)
+		{
+			printf("#");
+		}
+
+		printf("\n");
+	}
+
+	printf("\n");	
+
+	//Fourth piramid
+	for (int i = 0; i < heigth; i++)
+	{
+
+		//Writes " "
+        for (int j = 0; j < i ; j++)
+		{
+			printf(" ");
+		}
+
+		//Writes #
+		
+		for (int j = heigth+1; j > i ; j--)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
 
 }
